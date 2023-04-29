064bf0018c27c85cd66a6f5cfefab6ca0cea147b
Aktar Rajja Khan <aktarkhn07@gmail.com>
2023-04-29 19:36:55 +0530

    business checkpoint 7

--- src/main/scala/BusinessCheckpoint4.scala
+++ src/main/scala/BusinessCheckpoint4.scala
@@ -1,3 +1,17 @@
-object BusinessCheckpoint4 {
+
+import akka.actor.Actor
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+
+object BusinessCheckpoint4 extends App {
+  class ErrorHandler extends Actor {
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+    override def receive: Receive = {
+      case ex: Throwable =>
+        // Log the error
+        Source.single(ex).runWith(Sink.foreach(println))
+    }
+  }
 
 }
